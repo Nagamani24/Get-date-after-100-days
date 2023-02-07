5a623485eb068787d24cd4820730aa62289cf202
nagamani <b.n.mani2427@gmail.com>
2023-02-07 14:06:37 +0000

    Get date after 100 days

---
+++ app.js
@@ -0,0 +1,17 @@
+const addDays = require("date-fns/addDays");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  const dateAfterHundredDays = () => {
+    const newDate = addDays(new Date(), 100);
+
+    response.send(
+      `${newDate.getDate()}/${newDate.getMonth() + 1}/${newDate.getFullYear()}`
+    );
+  };
+  module.exports = dateAfterHundredDays;
+});
+
+module.exports = app;
