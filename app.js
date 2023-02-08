6874f53ef0fa6a0e82fc3c2653e813de5cb0ea38
Nagamani24 <124423255+Nagamani24@users.noreply.github.com>
2023-02-08 16:02:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  const dateAfterHundredDays = () => {
-    const newDate = addDays(new Date(), 100);
+    const todayDate = new Date();
+    const newDate = addDays(new Date(todayDate.getDate(),todayDate.getMonth(),todayDate.getFullYear()), 100);
 
     response.send(
       `${newDate.getDate()}/${newDate.getMonth() + 1}/${newDate.getFullYear()}`
     );
-  };
-  module.exports = dateAfterHundredDays;
+
 });
 
 module.exports = app;
